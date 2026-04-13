5f3583f617ef0f27086c708ac3973b27aa8d45ab
Bardawal Bihari <bardawal5555@gmail.com>
2026-04-14 00:24:52 +0530

    Resolve gradio vetical issue

--- gradio_app.py
+++ gradio_app.py
@@ -333,8 +333,6 @@ def get_themes(theme_list_str,subtitles_path,save_path):
         y="Score",
         title="Series Themes",
         tooltip=["Theme","Score"],
-        vertical = False,
-        vertical=False,
         width=500,
         height=260
     )
